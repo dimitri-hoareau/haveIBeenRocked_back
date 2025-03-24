cb745917e5073b3b0ba8a13822dd66f1790b532b
dimitri-hoareau <dimitrihoareau2@gmail.com>
2025-03-24 21:48:22 +0100

    import password into database optimisation

--- password_checker/scripts/import_rockyou.py
+++ password_checker/scripts/import_rockyou.py
@@ -1,8 +1,7 @@
 import os
 import django
 import hashlib
-from django.conf import settings
-from tqdm import tqdm
+from tqdm import tqdm  
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 django.setup()
@@ -10,20 +9,38 @@ django.setup()
 from password_checker.models import Password
 
 ROCKYOU_PATH = os.getenv("ROCKYOU_PATH", "data/rockyou.txt")
+BATCH_SIZE = 1000  
 
 def hash_password(password):
+    """Hash a password using SHA-1."""
     return hashlib.sha1(password.encode('utf-8')).hexdigest()
 
 def run():
+
+    bulk_list = []      
+    bulk_hashes = set() 
+
     with open(ROCKYOU_PATH, 'r', encoding='latin-1') as file:
         for line in tqdm(file, desc="Import progress", unit=" password"):
             password = line.strip()
             hash_value = hash_password(password)
 
-            if not Password.objects.filter(hash=hash_value).exists():
-                try:
-                    Password.objects.create(hash=hash_value)
-                except Exception as e:
-                    print(f"Error for {hash_value} : {e}")
+            if hash_value not in bulk_hashes and not Password.objects.filter(hash=hash_value).exists():
+                bulk_list.append(Password(hash=hash_value))
+                bulk_hashes.add(hash_value)
+
+                if len(bulk_list) >= BATCH_SIZE:
+                    try:
+                        Password.objects.bulk_create(bulk_list)
+                        bulk_list = []
+                        bulk_hashes = set()
+                    except Exception as e:
+                        print(f"Erreur d'insertion en bulk : {e}")
+
+    if bulk_list:
+        try:
+            Password.objects.bulk_create(bulk_list)
+        except Exception as e:
+            print(f"Error for bulk insertion : {e}")
 
     print("Import done successfully !")
